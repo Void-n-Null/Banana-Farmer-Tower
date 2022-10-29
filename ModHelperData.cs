3aaa10a1e9732aa8349583ecddae940aa5ec71c1
Void&Null <kosmix1999@gmail.com>
2022-10-29 16:32:09 -0700

    Fixed Description Issue

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BananaFarmer;
 public static class ModHelperData
 {
     public const string Name = "Banana Farmer Tower";
-    public const string Description = "Adds the Banana Farmer as a full tower with 3 upgrade paths and a a paragon.";
+    public const string Description = "Adds the Banana Farmer as a full tower with 3 upgrade paths and a paragon.";
     public const string Version = "2.0.1";
     public const string RepoName = "Banana-Farmer-Tower";
     public const string RepoOwner = "Void-n-Null";
