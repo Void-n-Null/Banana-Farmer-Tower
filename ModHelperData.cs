4eee3aef4147f63a85ac456ff4d3bba105a94446
Void&Null <kosmix1999@gmail.com>
2023-01-21 11:21:48 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string Name = "Banana Farmer Tower";
     public const string Description = "Adds the Banana Farmer as a full tower with 3 upgrade paths and a paragon.";
-    public const string Version = "2.1.0";
+    public const string Version = "2.1.1";
     public const string RepoName = "Banana-Farmer-Tower";
     public const string RepoOwner = "Void-n-Null";
     public const string DllName = "BananaFarmer.dll";
